d3698698ae91d01aed20d7f9e9a70f595d91081f
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-04-07 18:15:33 -0700

    Add files via upload

---
+++ SERVICES/SQL/SQL_MANAGER/SQL_AI_MANAGER/Sql_Ai_Manager01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EASY_CALLENDER01.SERVICES.SQL.SQL_MANAGER.SQL_AI_MANAGER
+{
+    internal class Sql_Ai_Manager01
+    {
+
+
+
+
+    }
+}
---
+++ SERVICES/SQL/SQL_SERVICES/SQL_FILE_SERVICES/Sql_File_Services01.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EASY_CALLENDER01.SERVICES.SQL.SQL_SERVICES.SQL_FILE_SERVICES
+{
+    public class Sql_File_Services01
+    {
+   
+    }
+}
