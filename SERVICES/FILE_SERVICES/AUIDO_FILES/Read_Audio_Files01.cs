ca13a927e37ac4a0940f88a18f6bb6b901b7702b
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-04-24 01:05:36 -0700

    Add files via upload

---
+++ SERVICES/FILE_SERVICES/AUIDO_FILES/Read_Audio_Files01.cs
@@ -0,0 +1,12 @@
+﻿using EASY_CALLENDER01.SERVICES.FILE_SERVICES.FILE_HELPER;
+
+namespace EASY_CALLENDER01.SERVICES.FILE_SERVICES.AUIDO_FILES
+{
+    public class Read_Audio_Files01
+    {
+        private static File_Helper01 File_H01 = new File_Helper01();
+        private static string[] file_helper = File_H01.all_embedded_audio_files().Split();
+        public string[] audio_files01 => file_helper;
+    }
+}
+
