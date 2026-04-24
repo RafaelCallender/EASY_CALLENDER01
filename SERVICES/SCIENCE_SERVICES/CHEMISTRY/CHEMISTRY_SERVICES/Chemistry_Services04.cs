1c187ec8a53594c7da62ed8dbc401cef4a2744ae
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-04-24 01:01:29 -0700

    Add files via upload

---
+++ SERVICES/SCIENCE_SERVICES/CHEMISTRY/CHEMISTRY_SERVICES/Chemistry_Services04.cs
@@ -0,0 +1,78 @@
+﻿using EASY_CALLENDER01.MODEL.SCIENCE_MODEL.CHEMISTRY_MODEL.CHEMISTRY_GET_MODEL;
+using EASY_CALLENDER01.SERVICES.SECURITY_SERVICES;
+using EASY_CALLENDER01.SERVICES.FILE_SERVICES.TEXT_FILES;
+using System.Threading.Tasks;
+using EASY_CALLENDER01.SERVICES.MYSQL.MYSQL_SERVICES.MYSQL_SCIENCE_SERVICES.MYSQL_CHEMISTRY_SERVICES;
+using EASY_CALLENDER01.SERVICES.SCIENCE_SERVICES.CHEMISTRY.CHEMISTRY_HELPER;
+
+namespace EASY_CALLENDER01.SERVICES.SCIENCE_SERVICES.CHEMISTRY.CHEMISTRY_SERVICES
+{
+    public class Chemistry_Services04
+    {
+private static  Chemistry_Get_Model01 Chemistry_Get_M01=new Chemistry_Get_Model01();  
+private static Chemistry_Helper01 Chemistry_H01=new Chemistry_Helper01();
+
+private static MySql_Chemistry_Services02 MySql_Chemistry_S02=new MySql_Chemistry_Services02();                
+
+              public bool insert_element_using_mysql()
+        {
+           string MysqlResult="";
+bool result = false;     
+
+
+      for (int i = 0; i < Chemistry_H01.atomic_number.Count; i++)
+                        {
+                                
+
+ result = MySql_Chemistry_S02.insert_element(
+                        Chemistry_H01.atomic_number[i],
+                        Chemistry_H01.element_name[i],
+                        Chemistry_H01.element_symboles[i],
+                        Chemistry_H01.atomic_mass[i],
+                        Chemistry_H01.protons[i],
+                        Chemistry_H01.electons[i],
+                        Chemistry_H01.neutrons[i],
+                        out MysqlResult);
+
+                        }
+
+ Chemistry_Get_M01.MySqlOutString=MysqlResult;
+return          result;                      
+
+        }
+
+
+
+        public string find_element_using_mysql(string input)
+        {
+            
+return "working on it";
+
+        }
+
+
+
+
+        public async Task<string> find_category_elements_using_mysql(int input)
+        {
+            
+return "working on it";
+
+
+        }
+
+ public async Task<string> view_all_elements_using_mysql()
+
+ {
+
+
+
+return "working on it";
+
+
+    
+ }
+
+     
+    }
+}
---
+++ SERVICES/SQLITE/SQLITE_MANAGER/SQLITE_LANGUAGE_MANGER/Sqlite_Lanaguge_Manager01.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EASY_CALLENDER01.SERVICES.SQLITE.SQLITE_MANAGER.SQL_LANGUAGE_MANGER
+{
+    public class Sqlite_Lanaguge_Manager01
+    {
+    }
+}
