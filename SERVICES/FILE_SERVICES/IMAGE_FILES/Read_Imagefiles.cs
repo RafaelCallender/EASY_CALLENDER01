abdfa833132f0a04655d468154b095f75608f52a
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-05-03 22:43:14 -0700

    Add files via upload

---
+++ SERVICES/FILE_SERVICES/IMAGE_FILES/Read_Imagefiles.cs
@@ -0,0 +1,27 @@
+
+using System.Net;
+using System.Net.Mail;
+
+namespace EASY_CALLENDER01.SERVICES.FILE_SERVICES.IMAGEFILE
+{
+    public class Read_Imagefiles
+    {
+		private static string[]data01_={
+		
+		    "~/IMAGES/cashapp01.jpg",
+			 "~/IMAGES/cashapp02.png",
+            "~/IMAGES/cashapp03.png",
+            "~/IMAGES/zelleimage01.png",
+            "~/IMAGES/zelleimages02.png",
+			"~/IMAGES/paypal01.png",
+			"~/IMAGES/paypal02.png",
+			"~/IMAGES/paypal03.png",
+			"~/IMAGES/venmo01.png",
+			"~/IMAGES/venmo02.png",
+			"~/IMAGES/venmo03.png"
+			
+		};
+		public string[]data01=>data01_;
+		
+	}
+}
---
+++ SERVICES/FILE_SERVICES/MOVIES_FILE/Read_Movies01.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_APP.SERVICES.FILE_SERVICES.MOVIES_FILE
+{
+    public class Read_Movies01
+    {
+
+    }
+}
